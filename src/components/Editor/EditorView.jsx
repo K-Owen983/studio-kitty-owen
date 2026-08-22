c0b1e900a3f0ae6aa9c78156236440fbfd5f861f
K-Owen983 <k.owendatascience@gmail.com>
2026-08-21 22:18:07 -0500

    Update EditorView.jsx

--- src/components/Editor/EditorView.jsx
+++ src/components/Editor/EditorView.jsx
@@ -21,6 +21,61 @@ import LivePreviewModal from '../Preview/LivePreviewModal';
 import { usePublications } from '../../context/PublicationContext';
 import { calculateReadingTime, generateSlug, extractExcerpt } from '../../utils/textHelpers';
 
+const FontSize = Extension.create({
+  name: 'fontSize',
+
+  addOptions() {
+    return {
+      types: ['textStyle'],
+    };
+  },
+
+  addGlobalAttributes() {
+    return [
+      {
+        types: this.options.types,
+        attributes: {
+          fontSize: {
+            default: null,
+
+            parseHTML: element => element.style.fontSize || null,
+
+            renderHTML: attributes => {
+              if (!attributes.fontSize) {
+                return {};
+              }
+
+              return {
+                style: `font-size: ${attributes.fontSize}`,
+              };
+            },
+          },
+        },
+      },
+    ];
+  },
+
+  addCommands() {
+    return {
+      setFontSize:
+        fontSize =>
+        ({ chain }) => {
+          return chain()
+            .setMark('textStyle', { fontSize })
+            .run();
+        },
+
+      unsetFontSize:
+        () =>
+        ({ chain }) => {
+          return chain()
+            .setMark('textStyle', { fontSize: null })
+            .run();
+        },
+    };
+  },
+});
+
 export default function EditorView() {
   const {
     currentPublication,
